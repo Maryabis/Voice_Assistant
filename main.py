fda45a8f6da607f3f86f4a645575a67c9d0d9a7d
Maryabis <150610695+Maryabis@users.noreply.github.com>
2025-03-13 21:48:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    command = ""  # Initialize command with an empty string
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+    except Exception as e:
+        print("Error:", e)  # Print error for debugging
+    return command  # Now 'command' is always returned
+
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'who the heck is' in command:
+        person = command.replace('who the heck is', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'date' in command:
+        talk('sorry, I have a headache')
+    elif 'are you single' in command:
+        talk('I am in a relationship with wifi')
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    elif 'stop' in command or 'exit' in command or 'goodbye' in command:
+        talk("Goodbye!")
+        exit() 
+    else:
+        talk('Please say the command again.')
+
+try:
+    while True:
+        run_alexa()
+except KeyboardInterrupt:
+    print("\nAlexa stopped.")
